f5677eca1a776f29b74baaf888005e8db457ffa9
mharoot <michaelharootoonyan@yahoo.com>
2018-11-11 11:30:48 -0800

    this is so nice i love it

--- sidebars/fancy/index.php
+++ sidebars/fancy/index.php
@@ -2,8 +2,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <link href="./style.css" rel="stylesheet" id="style-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    
 </head>
 
 <!------ Include the above in your HEAD tag ---------->
@@ -16,30 +17,44 @@
             <ul class="nav sidebar-nav">
                 <li class="sidebar-brand">
                     <a href="#">
-                       Brand
+                       Digital Decore
                     </a>
                 </li>
                 <li>
-                    <a href="#">Home</a>
-                </li>
-                <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#">Events</a>
-                </li>
-                <li>
-                    <a href="#v-2.0.1-activity-diagram-dd-file-intake.svg">v-2.0.1-activity-diagram-dd-file-intake.svg</a>
+                    <a href="#top">Software Engineer Artifacts</a>
                 </li>
                 <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Works <span class="caret"></span></a>
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">MIT Sprint Spreadsheets<span class="caret"></span></a>
                   <ul class="dropdown-menu" role="menu">
-                    <li class="dropdown-header">Dropdown heading</li>
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
+                    <li class="dropdown-header">Bi-Weekly Sprints</li>
+                    <li><a href="#sprint1">Sprint 1</a></li>
+                    <li><a href="#sprint2">Sprint 2</a></li>
+                    <li><a href="#sprint3">Sprint 3</a></li>
+                    <li><a href="#sprint4">Sprint 4</a></li>
+                    <li><a href="#sprint5">Sprint 5</a></li>
+                    <li><a href="#sprint6">Sprint 6</a></li>
+                    <li><a href="#sprint7">Sprint 7</a></li>
+                    <li><a href="#sprint8">Sprint 8</a></li>
+                  </ul>
+                </li>
+                <!-- <li>
+                    <a href="#">Sprint</a>
+                </li>
+                <li>
+                    <a href="#">Events</a> -->
+                </li>
+                <!-- <li>
+                    <a href="#v-2.0.1-activity-diagram-dd-file-intake.svg">Activity Diagram</a>
+                </li> -->
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">dd-file-intake Activity Diagrams<span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li class="dropdown-header">Version Snapshots</li>
+                    <li><a href="#v-2.0.1-activity-diagram-dd-file-intake.svg">v-2.0.1-latest</a></li>
+                    <li><a href="./dd-file-intake-activity-diagram-v-2.0.0.html">v-2.0.0</a></li>
+                    <!-- <li><a href="#">Something else here</a></li>
                     <li><a href="#">Separated link</a></li>
-                    <li><a href="#">One more separated link</a></li>
+                    <li><a href="#">One more separated link</a></li> -->
                   </ul>
                 </li>
                 <li>
@@ -57,21 +72,32 @@
 
         <!-- Page Content -->
         <div id="page-content-wrapper">
-            <button type="button" class="hamburger is-closed" data-toggle="offcanvas">
-                <span class="hamb-top"></span>
-    			<span class="hamb-middle"></span>
-				<span class="hamb-bottom"></span>
-            </button>
             <div class="container">
                 <div class="row">
                     <div class="col-lg-8 col-lg-offset-2">
-                        <h1>Fancy Toggle Sidebar Navigation</h1>
-                        <p>Bacon ipsum dolor sit amet tri-tip shoulder tenderloin shankle. Bresaola tail pancetta ball tip doner meatloaf corned beef. Kevin pastrami tri-tip prosciutto ham hock pork belly bacon pork loin salami pork chop shank corned beef tenderloin meatball cow. Pork bresaola meatloaf tongue, landjaeger tail andouille strip steak tenderloin sausage chicken tri-tip. Pastrami tri-tip kielbasa sausage porchetta pig sirloin boudin rump meatball andouille chuck tenderloin biltong shank </p>
-                        <p>Pig meatloaf bresaola, spare ribs venison short loin rump pork loin drumstick jowl meatball brisket. Landjaeger chicken fatback pork loin doner sirloin cow short ribs hamburger shoulder salami pastrami. Pork swine beef ribs t-bone flank filet mignon, ground round tongue. Tri-tip cow turducken shank beef shoulder bresaola tongue flank leberkas ball tip.</p>
-                        <p>Filet mignon brisket pancetta fatback short ribs short loin prosciutto jowl turducken biltong kevin pork chop pork beef ribs bresaola. Tongue beef ribs pastrami boudin. Chicken bresaola kielbasa strip steak biltong. Corned beef pork loin cow pig short ribs boudin bacon pork belly chicken andouille. Filet mignon flank turkey tongue. Turkey ball tip kielbasa pastrami flank tri-tip t-bone kevin landjaeger capicola tail fatback pork loin beef jerky.</p>
-                        <p>Chicken ham hock shankle, strip steak ground round meatball pork belly jowl pancetta sausage spare ribs. Pork loin cow salami pork belly. Tri-tip pork loin sausage jerky prosciutto t-bone bresaola frankfurter sirloin pork chop ribeye corned beef chuck. Short loin hamburger tenderloin, landjaeger venison porchetta strip steak turducken pancetta beef cow leberkas sausage beef ribs. Shoulder ham jerky kielbasa. Pig doner short loin pork chop. Short ribs frankfurter rump meatloaf.</p>
-                        <p>Filet mignon biltong chuck pork belly, corned beef ground round ribeye short loin rump swine. Hamburger drumstick turkey, shank rump biltong pork loin jowl sausage chicken. Rump pork belly fatback ball tip swine doner pig. Salami jerky cow, boudin pork chop sausage tongue andouille turkey.</p>                         
+                        <a name="top"></a>
+                        <h1> Digital Decore Software Engineering Artifacts </h1>
+                        <p> You will find Activity Diagrams, UMLs, Product Backlog, sprint spreadsheets, and other software engineering artifacts as they become available.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <button type="button" class="hamburger is-closed" data-toggle="offcanvas">
+                            <span class="hamb-top"></span>
+                            <span class="hamb-middle"></span>
+                            <span class="hamb-bottom"></span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
                         <a name="v-2.0.1-activity-diagram-dd-file-intake.svg"></a>
+                        <h1>Digital Decore File Intake activity diagram (Latest Version <b>2.0.1</b>):</h1>
+                        <p> We have been using this activity diagram since version 2.0.1 of the software. We are currently on version 2.0.5 of the software and still using the activity diagram below.</p>
                         <?php include_once "v-2.0.1-activity-diagram-dd-file-intake.svg"; ?>
                     </div>
                 </div>
